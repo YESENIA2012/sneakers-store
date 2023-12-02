135466ed3cf0c628813a267e940eacb8c47335d3
YESENIA2012 <gyesenia173@gmail.com>
2023-12-01 20:20:50 -0500

    createPricesSpecialsList was modified

--- src/controllers/pricesController.js
+++ src/controllers/pricesController.js
@@ -1,5 +1,5 @@
 const { initUsersModel } = require("../models/userModel")
-const Products = require("../models/productModel")
+const initProductsModel = require("../models/productModel")
 
 class PricesController {
   async getSpecialPrice(userId, productName) {
@@ -20,6 +20,7 @@ class PricesController {
     }
 
     if(!priceProduct){
+      const Products = await initProductsModel()
       const product = await Products.findOne({ nombre: productName }).lean();
       priceProduct = product.precioBase
     }
@@ -28,10 +29,28 @@ class PricesController {
   }
 
   async createPricesSpecialsList(dataClient){
-    const PricesModel = await initUsersModel()
-    const specialPricesList = PricesModel.create(dataClient)
+    const { nombre, nombre_producto, precio_especial_personal } = dataClient
+    
+    const dataClientSpecialProduct = {
+      nombre,
+      metadata: {
+        precios_especiales: [
+          {
+            nombre_producto,
+            precio_especial_personal
+          }
+        ],
+      }
+    }
 
-    return specialPricesList
+    const PricesModel = await initUsersModel()
+    const specialPricesList = new PricesModel(dataClientSpecialProduct)
+    const result = await specialPricesList.save()
+
+    return result
+
+    //voy haciendo el endpoint para consutrir la lista con los precios especiales del cliente
+    //acuerdece que si no ecnuentra en uno le toca ir a buscar en otro endpoint
   }
 }
 
