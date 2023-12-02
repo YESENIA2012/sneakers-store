068f3021a53e4091d29e63101ab2b35417fc0f8a
YESENIA2012 <gyesenia173@gmail.com>
2023-12-01 23:36:34 -0500

    PriceModel was rename to UserModel and some validations were added

--- src/controllers/pricesController.js
+++ src/controllers/pricesController.js
@@ -4,13 +4,14 @@ const initProductsModel = require("../models/productModel")
 class PricesController {
   async getSpecialPrice(userId, productName) {
     let priceProduct
-    const PricesModel = await initUsersModel()
+    const UserModel = await initUsersModel()
 
-    const productFind = await PricesModel.findOne({ _id: userId }).lean()
-    const priceList = productFind["metadata"]["precios_especiales"]
+    const productFind = await UserModel.findOne({ _id: userId }).lean()
+    const priceList = productFind["metadata"]
 
     if(priceList){
-      priceList.forEach(price => {
+      const priceListClient = priceList["precios_especiales"]
+      priceListClient.forEach(price => {
         const nameProduct = price.nombre_producto
 
         if(nameProduct == productName){
@@ -22,35 +23,44 @@ class PricesController {
     if(!priceProduct){
       const Products = await initProductsModel()
       const product = await Products.findOne({ nombre: productName }).lean();
+
+      if(!product){
+        console.log("priceProduct", priceProduct)
+        const error = new Error("Product not found");
+        error.statusCode = 404;
+        throw error
+      }
+
       priceProduct = product.precioBase
     }
 
     return priceProduct
   }
 
-  async createPricesSpecialsList(dataClient){
+  async createPricesSpecialsListForTesting(dataClient){
     const { nombre, nombre_producto, precio_especial_personal } = dataClient
-    
+
     const dataClientSpecialProduct = {
       nombre,
-      metadata: {
+    }
+    
+    if(nombre_producto || precio_especial_personal){
+
+      dataClientSpecialProduct.metadata = {
         precios_especiales: [
           {
             nombre_producto,
-            precio_especial_personal
-          }
+            precio_especial_personal,
+          },
         ],
-      }
+      };
     }
 
-    const PricesModel = await initUsersModel()
-    const specialPricesList = new PricesModel(dataClientSpecialProduct)
+    const UserModel = await initUsersModel()
+    const specialPricesList = new UserModel(dataClientSpecialProduct)
     const result = await specialPricesList.save()
 
     return result
-
-    //voy haciendo el endpoint para consutrir la lista con los precios especiales del cliente
-    //acuerdece que si no ecnuentra en uno le toca ir a buscar en otro endpoint
   }
 }
 
